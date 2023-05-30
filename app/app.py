d54b73954b00417c7d7a3fae08bbe24813a53e89
John Erlandsson <john.erlandsson@amokabel.com>
2023-05-30 08:11:29 +0200

    Fixed password bug

--- app/app.py
+++ app/app.py
@@ -8,15 +8,14 @@ app.logger.setLevel(logging.INFO)
 g_conn = None
 
 class Database:
-    def __init__(self, password_file=None):
+    def __init__(self, password_file):
         app.logger.info("Connecting to db")
-        with open(password_file, 'r') as pf:
-            pwd = pf.read()
-            app.logger.info("Password lol: %s" %pwd)
+        with open(password_file, 'r', newline='') as pf:
+            pwd = pf.read().rstrip('\n')
+
             self.connection = mysql.connector.connect(
                 user='root', 
-                # password=pwd,
-                password='19bananskal',
+                password=pwd,
                 host='db',
                 database='testdb',
                 auth_plugin='mysql_native_password'
@@ -32,7 +31,7 @@ def db_handle():
     global g_conn
     if not g_conn:
         try: 
-            g_conn = Database('/run/secrets/db-password')
+            g_conn = Database('/var/run/secrets/db-password')
         except mysql.connector.errors.ProgrammingError as e:
             app.logger.error(e.msg)
             g_conn = None
